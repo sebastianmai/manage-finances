bd524bcc09aba484a6a76980050d9bb103e0f5f4
Sebastian Mai <sebastian.mai@uni-konstanz.de>
2026-08-18 21:39:57 +0200

    add navbar and theme switch

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import sun from '../assets/sun_rm.svg';
+import moon from '../assets/moon_rm.svg';
+
+const navigation = [
+    { name: 'Dashboard', href: '#', current: true },
+]
+
+export default function Navbar({ theme, setTheme }) {
+
+    const handleClick = () => {
+
+        if (theme === 'dark') {
+            setTheme('light');
+        } else {
+            setTheme('dark');
+        }
+
+        console.log('Theme is:', theme);
+    }
+
+    return (
+        <div className="min-h-full bg-ui-light-bg">
+            <div className="hidden sm:flex sm:items-center sm:justify-between sm:ml-6 sm:mr-6 sm:py-3">
+                <div className="flex items-center space-x-4">
+                    {navigation.map((item) => (
+                        <button
+                            key={item.name}
+                            type="button"
+                            className="px-4 py-2 bg-ui-btn text-ui-btn-text rounded font-bold"
+                        >
+                            {item.name}
+                        </button>
+                    ))}
+                </div>
+
+                <button className="px-4 py-2" onClick={handleClick}>
+                    <img
+                        src={theme === 'dark' ? sun : moon}
+                        alt="Theme toggle"
+                        className="h-6 w-6"
+                        style={theme === 'dark' ? { filter: 'brightness(0) invert(1)' } : undefined}
+                    />
+                </button>
+            </div>
+        </div>
+    )
+}
