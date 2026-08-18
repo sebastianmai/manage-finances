a5faae1b8bc2b60cc040d8ce0f733f416669e71a
Sebastian Mai <sebastian.mai@uni-konstanz.de>
2026-08-18 21:39:57 +0200

    add navbar and theme switch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import {useState, useEffect} from 'react'
 
 function App() {
 
+  const [theme, setTheme] = useState('dark')
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
-    <>
-    smth
-    test 
-    </>
+    <div className='min-h-screen bg-ui-bg flex flex-col'>
+      <Navbar theme={theme} setTheme={setTheme}></Navbar>
+    </div>
   )
 }
 
