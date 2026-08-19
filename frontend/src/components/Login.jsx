253f4729c5862e56f7dcd58a263b8dab856fcc52
Sebastian Mai <sebastian.mai@uni-konstanz.de>
2026-08-19 13:59:08 +0200

    fix login ui

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -30,7 +30,7 @@ export default function Login() {
                 <hr className="my-5 mx-3 border-t border-ui-text opacity-40"></hr>
                 <div className="flex justify-center text-ui-text">
                     <a className="text-ui-text">
-                        No account yet? <NavLink to="/sign-up" className="text-ui-btn">Sign up here</NavLink>
+                        No account yet? <NavLink to="/sign-up" className="text-ui-signup-btn underline">Sign up here</NavLink>
                     </a>
                 </div>
                 
