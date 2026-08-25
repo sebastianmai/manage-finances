8262de93647e1a76b048fb3dfd6bacb47c088d8b
Sebastian Mai <sebastian.mai@uni-konstanz.de>
2026-08-25 14:53:38 +0200

    fix thickness of svg

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 export default function Profile({ theme, setTheme }) {
     return (
-        <NavLink to="/profile" className="px-4 py-2 inline-flex items-center">
+        <NavLink to="/profile" className="px-4 py-2 inline-flex font-bold items-center">
             <img src={profile} alt="Profile"
                     className="h-6 w-6"
                     style={theme === 'dark' ? { filter: 'brightness(0) invert(1)' } : undefined}
